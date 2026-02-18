cf17d494ae4a67f34fe6711b27dbb4361edbf478
cridgeon <cridgeon@athenadefence.ai>
2026-02-19 08:34:06 +1000

    FEAT: addded main include

---
+++ src/gl-basic.hpp
@@ -0,0 +1,17 @@
+#ifndef CRIDGEON_GL_BASIC_HPP
+#define CRIDGEON_GL_BASIC_HPP
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+
+namespace cridgeon
+{
+    #include "rendering_system.hpp"
+    #include "postprocessor.hpp"
+    #include "shader/all.hpp"
+    #include "framebuffer.hpp"
+} // namespace cridgeon
+
+
+#endif
